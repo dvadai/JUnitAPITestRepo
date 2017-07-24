5917d2f9775b5ad88d13973097c5df910d9b8b4c
Diana Vadai <diana.vadai@gmail.com>
2017-07-24 13:47:27 +0100

    Improvement refactoring

--- src/test/java/thisIsTheWebDriverPackage/FirstTest.java
+++ src/test/java/thisIsTheWebDriverPackage/FirstTest.java
@@ -12,13 +12,15 @@ public class FirstTest {
 
     public  static class FirstAutomationTest {
 
+        private static final String PHPTRAVELS_TITLE_TEXT = "PHPTRAVELS | TravelTechnologyPartner";
+
         @Test
         public void firefftest(){
             System.setProperty("webdriver.gecko.driver", "/Users/dia/Downloads/geckodriver");
             WebDriver driver = new FirefoxDriver();
 
             driver.navigate().to("http://www.phptravels.net/");
-            Assert.assertEquals("The title is not: PHPTRAVELS | Travel Technology Partner", "PHPTRAVELS | Travel Technology Partner", driver.getTitle());
+            Assert.assertEquals("The title is not: " + PHPTRAVELS_TITLE_TEXT, PHPTRAVELS_TITLE_TEXT, driver.getTitle());
 
             driver.close();
         }
